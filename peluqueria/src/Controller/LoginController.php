4b15067019c0c86fff7cd3d217ec723b4a142c4c
henadalGH <hrnnnadal@gmail.com>
2025-01-11 22:18:05 -0300

    Cuarto comit

--- peluqueria/src/Controller/LoginController.php
+++ peluqueria/src/Controller/LoginController.php
@@ -12,13 +12,15 @@ class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
         public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-            // get the login error if there is one
+    {  
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-            // last username entered by the user
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername, 'error' => $error,]);
+        'last_username' => $lastUsername,
+        'error' => $error,
+        ]);
     }
 
     #[Route('/logout', name: 'app_logout')]
